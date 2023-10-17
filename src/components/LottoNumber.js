9fea111f46f3cfa90e49551fb398b30ec9d1dde3
HEESUNae <heesun9456@gmail.com>
2023-10-17 18:05:09 +0900

    design: add lotto ball animation

--- src/components/LottoNumber.js
+++ src/components/LottoNumber.js
@@ -1,8 +1,17 @@
-import React, { useCallback } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
 import Typography from './Typography';
 
 export default function LottoNumber({ numbers }) {
+  const [viewHieght, setViewHieght] = useState(0);
+  const [animatedValue] = useState(new Animated.Value(1));
+
+  const translateY = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-viewHieght * 0.6, 0],
+  });
+
+  // 로또 공 색상
   const getRandomBackgroundColor = useCallback(() => {
     const randomNumber = Math.floor(Math.random() * 6);
     if (randomNumber === 0) return 'pink';
@@ -13,12 +22,34 @@ export default function LottoNumber({ numbers }) {
     if (randomNumber === 5) return 'orange';
   }, []);
 
+  // numbers 가 셋팅되고 나서 실행
+  useEffect(() => {
+    animatedValue.setValue(0); // 0으로 기초값 셋팅
+    Animated.timing(animatedValue, {
+      duration: 300,
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  }, [numbers]);
+
   return (
-    <View style={styles.lottoBallWrap}>
+    <View
+      style={[styles.lottoBallWrap]}
+      // width, height , x, y 정보제공
+      onLayout={({ nativeEvent }) => {
+        setViewHieght(nativeEvent.layout.height);
+      }}
+    >
       {numbers.map((item) => (
-        <View style={[{ backgroundColor: getRandomBackgroundColor() }, styles.lottoBall]} key={item}>
+        <Animated.View
+          style={[
+            { backgroundColor: getRandomBackgroundColor(), transform: [{ translateY: translateY }] },
+            styles.lottoBall,
+          ]}
+          key={item}
+        >
           <Typography color="white">{item}</Typography>
-        </View>
+        </Animated.View>
       ))}
     </View>
   );
